e54f4e369559100017cbc02c5c3e555e40399113
Ayan Poddar <ayanpoddar0@gmail.com>
2021-07-15 12:35:03 -0700

    Costume button ready

---
+++ SocketFileTransfer/CustomControl/CButton.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketFileTransfer.CustomControl
+{
+    public class CButton : Button
+    {
+        public CButton()
+        {
+            BackColor = SystemColors.Menu;
+            CResize();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            CResize();
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+
+            var rect = ClientRectangle;
+            var smallRect = new Rectangle(2, 2, rect.Width - 4, rect.Height - 4);
+            using (var path = new GraphicsPath())
+            using (var smallPath = new GraphicsPath())
+            using (var brash = new SolidBrush(BackColor))
+            using (var pen = new Pen(this.Parent.BackColor, 8))
+            {
+                path.StartFigure();
+                path.AddArc(rect.X, rect.Y, Width, Width, 180, 90);
+                path.AddArc(rect.Right - Width, rect.Y, Width, Width, 270, 90);
+                path.AddArc(rect.Right - Width, rect.Bottom - Width, Width, Width, 0, 90);
+                path.AddArc(rect.X, rect.Bottom - Width, Width, Width, 90, 90);
+                path.CloseFigure();
+                Region = new Region(path);
+
+                smallPath.StartFigure();
+                smallPath.AddArc(smallRect.X, smallRect.Y, Width, Width, 180, 90);
+                smallPath.AddArc(smallRect.Right - Width, smallRect.Y, Width, Width, 270, 90);
+                smallPath.AddArc(smallRect.Right - Width, smallRect.Bottom - Width, Width, Width, 0, 90);
+                smallPath.AddArc(smallRect.X, smallRect.Bottom - Width, Width, Width, 90, 90);
+                smallPath.CloseFigure();
+
+                pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                pevent.Graphics.FillPath(brash, smallPath);
+                pevent.Graphics.DrawPath(pen, smallPath);
+            }
+            DrawText(pevent);
+        }
+
+        private void DrawText(PaintEventArgs pevent)
+        {
+            var text = TextRenderer.MeasureText(Text, Font, new Size(ClientRectangle.Width, ClientRectangle.Height), TextFormatFlags.WordBreak);
+
+            var poient = new Point(Width / 2 - (text.Width / 2), Height / 2 - (text.Height / 2));
+            pevent.Graphics.DrawString(Text, Font, new SolidBrush(ForeColor), poient);
+        }
+
+        private void CResize()
+        {
+            if (Width != Height || Height != Width)
+            {
+                Height = Width;
+                Width = Height;
+            }
+            else
+            {
+                return;
+            }
+            Invalidate();
+        }
+    }
+}
--- SocketFileTransfer/Form1.Designer.cs
+++ SocketFileTransfer/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace SocketFileTransfer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cButton1 = new SocketFileTransfer.CustomControl.CButton();
+            this.SuspendLayout();
+            // 
+            // cButton1
+            // 
+            this.cButton1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cButton1.Location = new System.Drawing.Point(35, 187);
+            this.cButton1.Name = "cButton1";
+            this.cButton1.Size = new System.Drawing.Size(98, 98);
+            this.cButton1.TabIndex = 0;
+            this.cButton1.Text = "cButton1";
+            this.cButton1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(57)))), ((int)(((byte)(66)))));
+            this.ClientSize = new System.Drawing.Size(339, 513);
+            this.Controls.Add(this.cButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private CustomControl.CButton cButton1;
     }
 }
 
