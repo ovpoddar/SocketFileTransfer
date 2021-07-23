cbf3c3a17395c9c6603acee79a6fda961e3975ef
Ayan Poddar <ayanpoddar0@gmail.com>
2021-07-22 17:48:51 -0700

    Final with Ftp

--- SocketFileTransfer/Pages/ReceivedForm.cs
+++ SocketFileTransfer/Pages/ReceivedForm.cs
@@ -14,7 +14,39 @@ namespace SocketFileTransfer.Pages
 {
     public partial class ReceivedForm : Form
     {
-        
 
+        #region Connect To Another pc and handel the file transfar
+        private Socket _sarverSocket;
+        private Socket _clientSocket;
+        private byte[] _buffer;
+
+        public void OpenPortToConnect(IPEndPoint endPoint)
+        {
+            try
+            {
+                _sarverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _sarverSocket.Bind(endPoint);
+                _sarverSocket.Listen(10);
+                _sarverSocket.BeginAccept(AcceptClient, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AcceptClient(IAsyncResult ar)
+        {
+            _clientSocket = _sarverSocket.EndAccept(ar);
+            _buffer = new byte[_clientSocket.ReceiveBufferSize];
+            
+
+
+            _sarverSocket.BeginAccept(AcceptClient, null);
+        }
+
+        private void SendMessage(string message, FileTypes fileTypes)
+
+        #endregion
     }
 }
