0486b6efa84dfb050dd6dce19fcdd591de42b840
ovpoddar <ayanpoddar0@gmail.com>
2023-03-06 06:27:39 +0530

    update bit

---
+++ UtilitiTools/Models/FirewallRuleModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFirewallHelper;
+
+namespace UtilitiTools.Models;
+internal class FirewallRuleModel
+{
+    public FirewallRuleModel(IFirewallRule rule)
+    {
+        Name = rule.Name;
+        Profile = rule.Profiles;
+        Enable = rule.IsEnable;
+    }
+    public string Name { get; init; }
+    public FirewallProfiles Profile { get; init; }
+    public bool Enable { get; init; }
+}
