5ac0328e11cf26a59fbf9343e53eb82f5ffa9c5a
ovpoddar <ayanpoddar0@gmail.com>
2023-03-29 17:08:50 +0530

    Create the bases for tcp scan conversation

--- SocketFileTransfer/Canvas/ReceivedForm.cs
+++ SocketFileTransfer/Canvas/ReceivedForm.cs
@@ -1,7 +1,10 @@
-﻿using SocketFileTransfer.ExtendClass;
+﻿using Microsoft.VisualBasic.Devices;
+using SocketFileTransfer.ExtendClass;
 using SocketFileTransfer.Model;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
@@ -37,25 +40,42 @@ namespace SocketFileTransfer.Canvas
 
 		private void ReceivedForm_Load(object sender, EventArgs e)
 		{
-			//var wifiAddress = new WlanClient();
+			// all ipv4 of this pc
+			var _addresses = (from address in NetworkInterface.GetAllNetworkInterfaces()
+					.Where(a => a.OperationalStatus == OperationalStatus.Up
+						&& a.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+							  let networkInterfaceType = address.NetworkInterfaceType
+							  let b = address.GetIPProperties().UnicastAddresses
+							  from ip in b.Where(ip => ip.Address.AddressFamily == AddressFamily.InterNetwork)
+							  select (networkInterfaceType, ip));
+
 			var thread = new Thread(async () =>
-			{
-				// all ipv4 of this pc
-				var _addresses = NetworkInterface.GetAllNetworkInterfaces()
-					.Where(a => a.OperationalStatus == OperationalStatus.Up)
-					.SelectMany(a => a.GetIPProperties().UnicastAddresses)
-					.Where(a => a.Address.AddressFamily == AddressFamily.InterNetwork)
-					.Select(a => a.Address.ToString())
-					.ToList();
+			{				
 
-				//var addr = wifiAddress.Interfaces
-				//	.SelectMany(wlaninterfaces =>
-				//		wlaninterfaces.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllManualHiddenProfiles)
-				//		.Where(a => a.dot11DefaultAuthAlgorithm == Wlan.Dot11AuthAlgorithm.RSNA))
-				//	;
-
+				if (!_addresses.Any())
+					LblMsg.Text = "Faild To start Your hotspot. Please do it maually or connect your self with a network cable which is connected with router.";
+				else
+				{
+					BrodCastSignal();
+					LblMsg.Text = "waiting for user to connect";
+				}
 			});
 			thread.Start();
+		}
+
+		private void BrodCastSignal()
+		{
+			var tcpListner = new TcpListener(IPAddress.Any, 1400);
+			tcpListner.Start();
+			tcpListner.BeginAcceptTcpClient(BrodcastSignal, tcpListner);
+		}
+
+		private void BrodcastSignal(IAsyncResult ar)
+		{
+			var tcpListner = (TcpListener)ar.AsyncState;
+			var client = tcpListner.EndAcceptTcpClient(ar);
+			// call the taking about scanning and store the channel and keep farther listening for farther listening
+			tcpListner.BeginAcceptTcpClient(BrodcastSignal, tcpListner);
 		}
 
 		private void BtnBack_Click(object sender, EventArgs e)
---
+++ SocketFileTransfer/Model/TcpConnectionModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SocketFileTransfer.Model;
+public enum TcpConnectionModel : int
+{
+	Scaning = 0,
+	Connect = 1,
+	Finalized = 2,
+}
