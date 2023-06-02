5f761766e3773a2b1bb6b73c3e6c702a09f1817b
ovpoddar <ayanpoddar0@gmail.com>
2023-06-02 21:25:49 +0530

    clean up

--- SocketFileTransfer/Configuration/StaticConfiguration.cs
+++ SocketFileTransfer/Configuration/StaticConfiguration.cs
@@ -4,5 +4,4 @@ namespace SocketFileTransfer.Configuration;
 internal static class StaticConfiguration
 {
 	public static string StoredLocation = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-	public static byte[] NegotiationMessage = new byte[2] { 1, 1 };
 }
