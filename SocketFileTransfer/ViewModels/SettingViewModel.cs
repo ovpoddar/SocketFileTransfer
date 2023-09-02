e0809fcfc06ad44e4f950587050ecc6cfacd1dec
Ov poddar <ayanpoddar0@gmail.com>
2023-09-02 05:38:18 +0530

    added the setting viewmodel.

---
+++ SocketFileTransfer/ViewModels/SettingViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketFileTransfer.ViewModels;
+public class SettingViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+
+    private int _applicationRequiredPort;
+
+    public int ApplicationRequiredPort
+    {
+        get => _applicationRequiredPort;
+        set
+        {
+            _applicationRequiredPort = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ApplicationRequiredPort)));
+        }
+    }
+
+    private string _savePath;
+
+    public string SavePath
+    {
+        get => _savePath;
+        set
+        {
+            _savePath = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SavePath)));
+        }
+    }
+
+}
